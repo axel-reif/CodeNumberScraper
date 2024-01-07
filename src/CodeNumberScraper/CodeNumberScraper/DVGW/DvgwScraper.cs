a0ca8f68c83a743ee9782ea2b985cb07e648fb04
Axel Reif <axel.reif@axp-consulting.de>
2024-01-07 16:12:59 +0100

    - new data csv - fixed issue where the Accept-Language was not set to German and thus the data is given in English

--- src/CodeNumberScraper/CodeNumberScraper/DVGW/DvgwScraper.cs
+++ src/CodeNumberScraper/CodeNumberScraper/DVGW/DvgwScraper.cs
@@ -132,6 +132,9 @@ namespace CodeNumberScraper.DVGW
 
             using (var httpClient = CreateHttpClient())
             {
+                // Setzen des Accept-Language Headers auf 'de'
+                httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("de"));
+
                 string endpoint = "https://codevergabe.dvgw-sc.de/MarketParticipants/GetCodeCompanies";
 
                 string requestBody = @"draw=1&columns%5B0%5D%5Bdata%5D=Name&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=ZipCode&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=City&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=***LENGTH***&search%5Bvalue%5D=&search%5Bregex%5D=false&selectedMarketFunction=";
